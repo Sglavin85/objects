8b7e8ff81c2e4a6eaee890fb3eaa881ef73eeb5a
Sean Glavin <sean@Seans-MacBook-Pro.local>
2019-04-11 15:26:29 -0500

    lightning

---
+++ js/main.js
@@ -0,0 +1,74 @@
+const angus = {
+    age =1
+}
+
+const tess = {
+    age = 1
+}
+
+const food = {
+    type: "Hamburger ",
+    size: "small",
+    temperature: "Medium Rare"
+}
+
+const desk = {
+    height: 42,
+    width: 63,
+    depth: 30
+}
+
+const whiskers = {
+    species: "Cat",
+    name: "Whiskers",
+    age: 4,
+    color: "Black"
+}
+
+const dozer = {
+    species: "Dog",
+}
+
+
+// Lightning Round 1
+const mustang = {
+    make: "Ford",
+    model: "Mustang",
+    color: "red",
+    year: 2015
+}
+
+// Lightning Round 2
+const animals = [Kippers, Jack, Gypsy, Angus, Seymour Bouts]
+
+// Lightning Round 3
+const allFamily = {
+    const Sean = {
+        Gender: "male";
+        age: 33;
+        height: 72;
+        weight: 200;
+    }
+
+    const Chris = {
+        Gender: "male"
+        age: "75";
+        height: 75;
+        weight: 205;
+    }
+
+    const Stacy = {
+        Gender: "female";
+        age: 73;
+        height: 67;
+        weight: 121
+    }
+
+    const Matt = {
+        Gender: "male";
+        age: 65;
+        height: 76
+        width: 
+    }
+
+}
